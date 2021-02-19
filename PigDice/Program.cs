4658bc1ac12538387298eb58b5cc01822ad4cb6d
mathews225 <petemathews14@gmail.com>
2021-02-19 14:15:35 -0500

    Add project files.

---
+++ PigDice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PigDice {
+	class Program {
+
+		// Rules: Roll a die and add the amount to the running score. Turn continues until a 1 is rolled.
+		
+			static int RollDie() {
+			var rand = new Random();
+			return rand.Next(1, 7);
+		}
+		static int PlayPigDice() {
+			var score = 0;
+			var roll = RollDie();
+			while (roll != 1) {
+				score += roll;
+				roll = RollDie();
+			}
+			return score;
+
+		}
+
+		static void Main(string[] args) {
+
+			var hiscore = 0;
+
+			for (int i = 0; i < 1000000; i++) {
+				var score = PlayPigDice();
+				if (score > hiscore) {
+					hiscore = score;
+				}
+			}
+			Console.WriteLine($"Score = {hiscore}");
+
+			//var score = PlayPigDice();
+			//Console.WriteLine($"Score = {score}");
+
+		}
+
+
+	}
+}
