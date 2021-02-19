f46e0ca6ece34756b14c05d3f58a17dbf4f8a7ed
mathews225 <petemathews14@gmail.com>
2021-02-19 14:26:32 -0500

    time to run

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -4,8 +4,19 @@ namespace PigDice {
 	class Program {
 
 		// Rules: Roll a die and add the amount to the running score. Turn continues until a 1 is rolled.
-		
-			static int RollDie() {
+
+		static DateTime getStartTime() {
+			var start = DateTime.Now;
+			return start;
+		}
+
+
+		static DateTime getEndTime() {
+			var end = DateTime.Now;
+			return end;
+		}
+
+		static int RollDie() {
 			var rand = new Random();
 			return rand.Next(1, 7);
 		}
@@ -23,14 +34,20 @@ namespace PigDice {
 		static void Main(string[] args) {
 
 			var hiscore = 0;
-
+			var start = getStartTime();
+			Console.WriteLine(start);
 			for (int i = 0; i < 1000000; i++) {
 				var score = PlayPigDice();
 				if (score > hiscore) {
 					hiscore = score;
 				}
 			}
+			var end = getEndTime();
 			Console.WriteLine($"Score = {hiscore}");
+			Console.WriteLine(end);
+
+
+
 
 			//var score = PlayPigDice();
 			//Console.WriteLine($"Score = {score}");
